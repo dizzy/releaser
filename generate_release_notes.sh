e0c8e29fb6087152afbb03ea31da52e73a4331b4
dizzy <george@ezuce.com>
2017-05-16 18:44:48 +0300

    Test

---
+++ generate_release_notes.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+firstTag=$(git tag | sort -r | head -1)
+secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
+git-release-notes ${secondTag}..${firstTag} > changelog-${firstTag}.html
