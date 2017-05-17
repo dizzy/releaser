3c73b672a1e4563573ff79985c3b5c0c3daaa37f
dizzy <george@ezuce.com>
2017-05-17 22:20:44 +0300

    upload some files to s3 bucket

--- generate_release_notes.sh
+++ generate_release_notes.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env sh
 
-firstTag=$(git tag | sort -r | head -1)
 secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-git-release-notes ${secondTag}..${firstTag} html > changelog-${firstTag}.html
+git-release-notes ${secondTag}..${RELEASE_TAG} html > changelog-${RELEASE_TAG}.html
