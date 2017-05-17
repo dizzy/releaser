2c92c55b38224c0376ff8ec6546b23eaca71e25a
dizzy <george@ezuce.com>
2017-05-17 10:48:32 +0300

    Fix release notes

--- generate_release_notes.sh
+++ generate_release_notes.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env sh
 
-cd awesome-project
 firstTag=$(git tag | sort -r | head -1)
 secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-git-release-notes ${secondTag}..${firstTag} > changelog-${firstTag}.html
-cd ..
+git-release-notes ${secondTag}..${firstTag} html > changelog-${firstTag}.html
