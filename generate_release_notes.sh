db53a479af223a62a41d2248a0a7cafa37a513a8
dizzy <george@ezuce.com>
2017-05-17 23:04:08 +0300

    cleanup

--- generate_release_notes.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-firstTag=$(git tag | sort -r | head -1)
-secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-echo "git-release-notes ${secondTag}..${firstTag} html > changelog-${firstTag}.html"
-git-release-notes ${secondTag}..${firstTag} html > changelog-${firstTag}.html
